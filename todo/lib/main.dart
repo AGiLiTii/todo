d41a488767a21abc407955a8b4eb6af9cdf0e735
Stefan Visser Russouw <24715034@sun.ac.za>
2023-12-07 11:55:29 +0200

    Added Functionality: Added a remove and add button to remove and add todos, can also now specify the name of the todo, added checkboxes to the todos that work.

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -24,7 +24,41 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyAppState extends ChangeNotifier {}
+class MyAppState extends ChangeNotifier {
+  // item Count that can change
+  List todoItems = [];
+  List listItems = [];
+
+  //Method to add todo
+  void addTodo(String name) {
+    if (todoItems.length <= 12) {
+      todoItems.add(TodoItem(name: name));
+    }
+    notifyListeners();
+  }
+
+// Add list
+  void addList(String name) {
+    if (listItems.length <= 12) {
+      listItems.add(TodoItem(name: name));
+    }
+    notifyListeners();
+  }
+
+  //Method to remove todo
+  void removeTodo(int index) {
+    if (index >= 0 && index < todoItems.length) {
+      todoItems.removeAt(index);
+    }
+    notifyListeners();
+  }
+}
+
+class TodoItem {
+  final String name;
+
+  TodoItem({required this.name});
+}
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -41,16 +75,19 @@ class _MyHomePageState extends State<MyHomePage> {
       case 0:
         page = TodoList();
       case 1:
-        page = Placeholder();
+        page = ListPage();
       default:
         page = Placeholder();
     }
+
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
+        floatingActionButton: AddButton(),
         appBar: AppBar(
-            backgroundColor: Colors.deepPurpleAccent,
-            title: Text('TODO App',
-                style: Theme.of(context).textTheme.headlineMedium)),
+          backgroundColor: Colors.deepPurpleAccent,
+          title: Text('TODO App',
+              style: Theme.of(context).textTheme.headlineMedium),
+        ),
         body: Row(
           children: [
             SafeArea(
@@ -88,23 +125,129 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class TodoList extends StatelessWidget {
+// Todolist
+class TodoList extends StatefulWidget {
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  List<bool> checkboxValues = List.generate(14, (index) => false);
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        for (var i = 0; i < 10; i++)
-          ListTile(
-            // Add leading checkbox
-            leading: Checkbox(
-              value: false,
-              onChanged: (bool? value) {},
-            ),
-            title:
-                // TODO: Recieve actual name
-                Text('Todo $i', style: Theme.of(context).textTheme.bodyMedium),
+    var appState = context.watch<MyAppState>();
+    return ListView.builder(
+      itemCount: appState.todoItems.length,
+      itemBuilder: (context, index) => TodoListItem(
+        index: index,
+        text: appState.todoItems[index].name,
+        isChecked: checkboxValues[index],
+        onCheckbocChanged: (bool? value) {
+          setState(() {
+            checkboxValues[index] = value!;
+          });
+        },
+      ),
+    );
+  }
+}
+
+//TODO: Add list button
+
+class ListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('This is the List Page'),
+    );
+  }
+}
+
+//Add a button right bottom corner
+class AddButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        _showAddTodoDialog(context);
+      },
+      child: Icon(Icons.add),
+    );
+  }
+
+  void _showAddTodoDialog(BuildContext context) {
+    TextEditingController textController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add TODO'),
+          content: TextField(
+            controller: textController,
+            decoration: InputDecoration(labelText: 'Enter TODO name'),
           ),
-      ],
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                String todoName = textController.text;
+                if (todoName.isNotEmpty) {
+                  // Add the TODO with the specified name
+                  Provider.of<MyAppState>(context, listen: false)
+                      .addTodo(todoName);
+                  Navigator.of(context).pop(); // Close the dialog
+                }
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class TodoListItem extends StatelessWidget {
+  final int index;
+  final bool isChecked;
+  final ValueChanged<bool?> onCheckbocChanged;
+  final String text;
+
+  const TodoListItem({
+    required this.index,
+    required this.isChecked,
+    required this.onCheckbocChanged,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Checkbox(
+        value: isChecked,
+        onChanged: onCheckbocChanged,
+      ),
+      title: Text(
+        text,
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
+      trailing: Icon(
+        Icons.delete,
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      onTap: () {
+        //uncheck checkbox
+        onCheckbocChanged(false);
+        // remove
+        Provider.of<MyAppState>(context, listen: false).removeTodo(index);
+      },
     );
   }
 }
